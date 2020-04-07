94d76aa61e93d6121129e515335ecede09c54312
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2020-04-07 17:47:21 +0200

    New mobile project

---
+++ Cook-Book-Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Cook_Book_Mobile.Services;
+using Cook_Book_Mobile.Views;
+
+namespace Cook_Book_Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
