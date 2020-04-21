58eeb740dce2263c578487c1f938e42a530a2ada
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2020-04-21 20:39:59 +0200

    Working on displaying public recipes

---
+++ Cook-Book-Mobile/Helpers/BooleanToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Cook_Book_Mobile.Helpers
+{
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                {
+                    return Color.Green;
+                }
+            }
+            return Color.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
