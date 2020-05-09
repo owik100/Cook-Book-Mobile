ba3726b373fb7d14006660677f81fc174b977d8b
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2020-05-09 15:41:49 +0200

    Triggers view and temp image picker fix

--- Cook-Book-Mobile.Android/MainActivity.cs
+++ Cook-Book-Mobile.Android/MainActivity.cs
@@ -65,25 +65,36 @@ namespace Cook_Book_Mobile.Droid
 
         private string GetPathToImage(Android.Net.Uri uri)
         {
-            string doc_id = "";
-            using (var c1 = ContentResolver.Query(uri, null, null, null, null))
-            {
-                c1.MoveToFirst();
-                string document_id = c1.GetString(0);
-                doc_id = document_id.Substring(document_id.LastIndexOf(":") + 1);
-            }
-
             string path = null;
-
-            // The projection contains the columns we want to return in our query.
-            string selection = Android.Provider.MediaStore.Images.Media.InterfaceConsts.Id + " =? ";
-            using (var cursor = ContentResolver.Query(Android.Provider.MediaStore.Images.Media.ExternalContentUri, null, selection, new string[] { doc_id }, null))
+            try
             {
-                if (cursor == null) return path;
-                var columnIndex = cursor.GetColumnIndexOrThrow(Android.Provider.MediaStore.Images.Media.InterfaceConsts.Data);
-                cursor.MoveToFirst();
-                path = cursor.GetString(columnIndex);
+                string doc_id = "";
+                using (var c1 = ContentResolver.Query(uri, null, null, null, null))
+                {
+                    c1.MoveToFirst();
+                    string document_id = c1.GetString(0);
+                    doc_id = document_id.Substring(document_id.LastIndexOf(":") + 1);
+                }
+
+              
+
+                // The projection contains the columns we want to return in our query.
+                string selection = Android.Provider.MediaStore.Images.Media.InterfaceConsts.Id + " =? ";
+                using (var cursor = ContentResolver.Query(Android.Provider.MediaStore.Images.Media.ExternalContentUri, null, selection, new string[] { doc_id }, null))
+                {
+                    if (cursor == null) return path;
+                    var columnIndex = cursor.GetColumnIndexOrThrow(Android.Provider.MediaStore.Images.Media.InterfaceConsts.Data);
+                    cursor.MoveToFirst();
+                    path = cursor.GetString(columnIndex);
+                }
+               
             }
+            catch (Exception ex)
+            {
+
+               //
+            }
+
             return path;
         }
     }
